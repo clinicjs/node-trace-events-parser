e773c5b90622ca4fe8aa617b9fec5f4697cc18ad
Fiacc <fiacc@users.noreply.github.com>
2018-08-29 17:01:08 -0700

    Fix (#1)
    * Fix
    
    * reset version

--- test.js
+++ test.js
@@ -1,9 +1,45 @@
 const tape = require('tape')
 const parser = require('./')
 
+tape('basic nested', function (t) {
+  const parse = parser()
+  const expected = [{ a: true, b: {} }, { a: false, b: { c: {} } }]
+  const testData = '{"traceEvents":[{"a":true,"b":{}},{"a":false,"b":{"c":{}}}]}'
+  parse.write(testData)
+  parse.end()
+
+  parse
+    .on('data', function (data) {
+      t.same(data, expected.shift())
+    })
+    .on('end', function () {
+      t.same(expected.length, 0)
+      t.end()
+    })
+})
+
+tape('chunked nested', function (t) {
+  const parse = parser()
+  const expected = [{ a: true, b: {} }, { a: false, b: { c: {} } }]
+  const testData = '{"traceEvents":[{"a":true,"b":{}},{"a":false,"b":{"c":{}}}]}'
+  for (var i = 0; i < testData.length; i++) {
+    parse.write(testData.slice(i, i + 1))
+  }
+  parse.end()
+
+  parse
+    .on('data', function (data) {
+      t.same(data, expected.shift())
+    })
+    .on('end', function () {
+      t.same(expected.length, 0)
+      t.end()
+    })
+})
+
 tape('basic', function (t) {
   const parse = parser()
-  const expected = [{a: true, b: {}}, {a: false, b: {}}]
+  const expected = [{ a: true, b: {} }, { a: false, b: {} }]
 
   parse.write('{"traceEvents":[{"a":true,"b":{}},{"a":false,"b":{}}]}')
   parse.end()
@@ -20,7 +56,7 @@ tape('basic', function (t) {
 
 tape('chunked', function (t) {
   const parse = parser()
-  const expected = [{a: true, b: {}}, {a: false, b: {}}]
+  const expected = [{ a: true, b: {} }, { a: false, b: {} }]
   const s = '{"traceEvents":[{"a":true,"b":{}},{"a":false,"b":{}}]}'
 
   for (var i = 0; i < s.length; i++) {
